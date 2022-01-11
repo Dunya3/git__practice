226cd2efe22d40b1c9cb8e840563d0399ad4ce3e
Dunja <dunjaberic@hotmail.com>
2022-01-11 13:16:52 -0500

    added file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,16 @@
 public class HelloWorld {
 
-    //Doing some work here
-
-    //Doing awesome work2!
-
-    //Doing awesome work3!
-    //Ready to be committed
-    //and ready to push
+    public static void main(String[] args) {
 
 
+        //Doing some work here
 
+        //Doing awesome work2!
+
+        //Doing awesome work3!
+        //Ready to be committed
+        //and ready to push
+
+
+    }
 }
